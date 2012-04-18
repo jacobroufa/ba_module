1397700d0c740cf0512c8fcc61a6245e7dae48be
Jacob Roufa <jacob.roufa@gmail.com>
2012-04-18 13:28:53 -0700

    Add beginner tutorials block

---
+++ tpl/beginner_tutorials.tpl.php
@@ -0,0 +1,9 @@
+<div id="beginner-tutorials">
+  <div class="beginner-tutorials">
+    <a id="previous" href="#">Previous</a>
+    <ul class="tutorial-list">
+      <?php print $content ?>
+    </ul>
+    <a id="next" href="#">Next</a>
+  </div>
+</div>
